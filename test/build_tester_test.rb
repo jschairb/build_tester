8144b2638a0f403bf3c04c3406c350b9f4eafdce
Josh Schairbaum <joshua.schairbaum@gmail.com>
2009-02-02 11:24:03 -0500

    adding new test method

--- test/build_tester_test.rb
+++ test/build_tester_test.rb
@@ -12,4 +12,9 @@ class BuildTesterTest < Test::Unit::TestCase
     @build_tester = BuildTester.new
     assert_equal false, @build_tester.return_false
   end
+
+  def test_should_instantiate_proper_class
+    @build_tester = BuildTester.new
+    assert_kind_of BuildTester, @build_tester
+  end
 end
